790fdf50cc5e4ca7b767db0f540085d6b73692c9
randeep2104 <ranadeep2104@gmail.com>
2023-08-20 21:06:45 +0530

    r-1.3.1

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,16 @@
 import { Inter } from 'next/font/google'
 import Box from '../components/Box'
-
+import Head from 'next/head'
+import logo from '/public/favicon.ico'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <main>
+      <Head>
+        <link rel='icon' type='image/x-icon' href={logo.src} ></link>
+        <title>Project-Puzzle</title>
+      </Head>
         <Box></Box>
     </main>
   )
